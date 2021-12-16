d1133048c172f09750e8a88c6fb2d644ef099e74
Johng117 <83654331+Johng117@users.noreply.github.com>
2021-12-16 14:40:19 +0000

    Added styles and functionality.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [allQuotes, setAllQuotes] = useState([]);
+  const [singleQuote, setSingleQuote] = useState({
+    quote: "Eighty percent of success is showing up.",
+    author: "Woody Allen",
+  });
+
+  // Fetch the quotes
+  useEffect(() => {
+    fetch("https://johngorman-quote-server.glitch.me/quotes")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setAllQuotes(data);
+      });
+  }, []);
+
+  const randomQuote = () => {
+    // Get and assign a random quote to 'rand'
+    let rand = Math.floor(Math.random() * allQuotes.length);
+    return setSingleQuote({
+      quote: allQuotes[rand].quote,
+      author: allQuotes[rand].author,
+    });
+  };
+
+  //  const rollingQuote=()=> {
+  //    setInterval(randomQuote, 3000)
+  //  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote-container">
+      <button onClick={randomQuote}>Next</button>
+      <div className="quote">
+        <h1>{singleQuote.quote}</h1>
+        <h2>{singleQuote.author}</h2>
+      </div>
     </div>
   );
 }
